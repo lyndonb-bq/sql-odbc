6a4acba8c28ac555c5717b7b99ab222378b975cc
lyndonb-bq <lyndonb@bitquilltech.com>
2020-05-07 14:56:46 -0700

    [1] trying to unhack and revert some additional changes that were not required

--- aws_sdk_cpp_setup.sh
+++ aws_sdk_cpp_setup.sh
@@ -16,26 +16,7 @@
 #
 
 cd src
-
 git clone "https://github.com/aws/aws-sdk-cpp.git"
-
 prefix_path=$(pwd)
 cd aws-sdk-cpp
-
-pwd
-
-ls -l
-
 cd ..
-
-cd ..
-
-# mkdir sdk-build
-
-# cd sdk-build
-
-# cmake ../aws-sdk-cpp/ -D CMAKE_INSTALL_PREFIX=$prefix_path/AWSSDK/ -D CMAKE_BUILD_TYPE=Release -D BUILD_ONLY="core" -D CUSTOM_MEMORY_MANAGEMENT="OFF" -D ENABLE_RTTI="OFF" -D ENABLE_TESTING="OFF"
-
-# make 
-
-# make install 
